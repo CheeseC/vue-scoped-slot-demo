226c6138cc9e281a545da1ec2829194ef68702d6
chenqi <1311949161@qq.com>
2018-12-31 14:42:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <child>
+      <template slot-scope="slotProps">
+        <ul>
+          <li v-for="item in slotProps.data" :key="item">{{item}}</li>
+        </ul>
+      </template>
+    </child>
+  </div>
+</template>
+
+<script>
+import child from './child.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    child
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <slot :data="items"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: ['测试1', '测试2', '测试3']
+    }
+  }
+}
+</script>
+
